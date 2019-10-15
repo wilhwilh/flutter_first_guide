de28904467a9f7eb6803e3f7ed8198f802081391
Flankorn <flankorn1@gmail.com>
2019-10-14 22:37:16 -0400

    basic manage of state

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,24 @@ import 'package:flutter/material.dart';
 // For function witch only have one and exactly expression
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+
+   @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
   void answerQuestion() {
-    print('Answer Choosen!');
+    // function to force re render of ui
+    setState(() {
+      questionIndex = questionIndex + 1;
+    });
+    print(questionIndex);
   }
 
   @override
@@ -25,7 +40,7 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Text('The question!'),
+            Text(questions[questionIndex]),
             RaisedButton(
               child: Text('Answer 1'),
               onPressed: answerQuestion,
