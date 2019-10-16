bb8df4610bc6aa532549069a8836ec441952fc75
Flankorn <flankorn1@gmail.com>
2019-10-15 21:09:22 -0400

    Added basic style to question widget

--- lib/question.dart
+++ lib/question.dart
@@ -8,6 +8,16 @@ class Question extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text(questionText);
+    return Container(
+      // this made that the container take all width of screen
+      width: double.infinity,
+      //  (((child(= Content) Padding) Border) Margin)
+      margin: EdgeInsets.all(10),
+      child: Text(
+        questionText,
+        style: TextStyle(fontSize: 28),
+        textAlign: TextAlign.center,
+      ),
+    );
   }
 }
